0e859a83eccefb7be06b6ffec5888a81f4fbb00b
Tomasz Bielak <bielak@people.pl>
2021-12-18 13:51:27 +0100

    whitespaces update

--- sources/2021/2021_12.h
+++ sources/2021/2021_12.h
@@ -5,11 +5,11 @@
 
 namespace Day12_2021
 {
-	typedef vector<string> t_destinations;			// list of destination nodes
-	typedef map<string, t_destinations> t_graph;	// <node, destintion nodes>
-	typedef pair<int, int> t_visitedpair;			// <number of visits, number of visits allowed>
-	typedef map<string, t_visitedpair> t_visited;	// <node, visited information>
-	typedef set<string> t_allpaths;					// all paths found
+	typedef vector<string> t_destinations;           // list of destination nodes
+	typedef map<string, t_destinations> t_graph;     // <node, destintion nodes>
+	typedef pair<int, int> t_visitedpair;            // <number of visits, number of visits allowed>
+	typedef map<string, t_visitedpair> t_visited;    // <node, visited information>
+	typedef set<string> t_allpaths;                  // all paths found
 
 	t_output main(const t_input& input);
 }
