e9e51b9b216f806ea49bc8f40293aeb492b33250
Tomasz Bielak <bielak@people.pl>
2022-01-25 11:59:53 +0100

    obsolete file removed

--- sources/shared/input_output.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INPUT_OUTPUT_H__
-#define __INPUT_OUTPUT_H__
-
-#include "stl.h"
-
-typedef vector<string> t_input;  // sequence of lines read from text file
-typedef pair<t_input, double> t_output;  // output lines + execution time in milliseconds
-
-#endif
